0b89fe01adc7cfa39ab84444a0574e4b34f9e98e
SamirAryamane <saryamane@salesforce.com>
2014-11-20 23:18:56 -0800

    Adding more examples for the decorator code along with wraps from the functools

--- decorator/decorator_python.py
+++ decorator/decorator_python.py
@@ -32,3 +32,64 @@ def hello_world():
 
 var = hello_world()
 print var
+
+
+# More examples of decorator within python
+
+def p_decorate(func):
+	def func_wrapper(name):
+		return "<p>{0}</p>".format(func(name))
+
+	return func_wrapper
+
+def strong_decorate(func):
+	def func_wrapper(name):
+		return "<strong>{0}</strong>".format(func(name))
+
+	return func_wrapper
+
+def div_decorate(func):
+	def func_wrapper(name):
+		return "<div>{0}</div>".format(func(name))
+
+	return func_wrapper
+
+@div_decorate
+@strong_decorate
+@p_decorate
+
+def new_get_text(name):
+	return "lorem ipsum, {0} dolor sit amet".format(name)
+
+print new_get_text("Samir")
+
+# Here the order of setting oir decorator does matter and is goes top down
+
+# A better way instead of defining 3 differnt functions, use a tag.
+
+from functools import wraps
+
+def tags(tag_name):
+	def tags_decorator(func):
+		@wraps(func)
+		def func_wrapper(name):
+			return "<{0}>{1}</{0}>".format(tag_name, func(name))
+
+		return func_wrapper
+	return tags_decorator
+
+@tags("p")
+
+def newest_get_text(name):
+	"""returns some text"""
+	return "Hello " +name
+
+print newest_get_text("Samir")
+print newest_get_text.__doc__
+print newest_get_text.__name__
+print newest_get_text.__module__
+print "This is the site where this exercise was picked from"
+
+print
+
+print "http://thecodeship.com/patterns/guide-to-python-function-decorators/"
